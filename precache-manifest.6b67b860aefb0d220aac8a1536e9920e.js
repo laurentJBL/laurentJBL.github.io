3db0b889251d2104023dba28a510f7cabe8bbc83
laurentJBL <68747133+laurentJBL@users.noreply.github.com>
2020-07-24 17:55:16 +0200

    Add files via upload

---
+++ precache-manifest.6b67b860aefb0d220aac8a1536e9920e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b93cedce279e863468d08b99434fcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "885812b63b8cec769242",
+    "url": "/static/css/main.a03d190b.chunk.css"
+  },
+  {
+    "revision": "93f0d0a5d6b7a27dc142",
+    "url": "/static/js/2.eef6bce4.chunk.js"
+  },
+  {
+    "revision": "885812b63b8cec769242",
+    "url": "/static/js/main.05789f99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5efeae11c2e17e6913c043f1a90deced",
+    "url": "/static/media/logomzk.5efeae11.png"
+  },
+  {
+    "revision": "feb2d7ab9628f5c818b22099c0cd9ed7",
+    "url": "/static/media/skycash.feb2d7ab.jpg"
+  }
+]);
